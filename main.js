a602b47aadd66eb452159f3749210100deaa7193
Ishan Menezes <46503433+Ishan-Menezes@users.noreply.github.com>
2023-02-11 18:38:26 +0400

    Update main.js
    Changed full code

--- main.js
+++ main.js
@@ -5,77 +5,85 @@ const default_text = "The Rizzler";
 init();
 
 function init() {
-	let text = sessionStorage.getItem("text");
-	if (text == null || text.length == 0)
-		text = default_text;
-	else
-		customization_input.value = text;
+    let text = sessionStorage.getItem("text");
+    if (text == null || text.length == 0)
+        text = default_text;
+    else
+        customization_input.value = text;
 
-	customization_output.textContent = text;
+    customization_output.textContent = text;
 }
 
 function update_text() {
-	const text = customization_input.value;
+    const text = customization_input.value;
 
-	if (text.length == 0)
-		text = default_text
+    if (text.length == 0)
+        text = default_text
 
-	sessionStorage.setItem("text", text);
-	customization_output.textContent = text;
-}
-
-function update_rotation() {
-	angle = (
-		document
-			.querySelector("#rotation_slider")
-			.value
-	);
-	document.querySelector("#preview").style.transform = `rotate(${angle}deg)`;
+    sessionStorage.setItem("text", text);
+    customization_output.textContent = text;
 }
 
 function buy() {
-	let text = customization_input.value;
-	sessionStorage.setItem("text", text);
+    let text = customization_input.value;
+    sessionStorage.setItem("text", text);
 
-	if (text.length == 0)
-		text = default_text;
+    if (text.length == 0)
+        text = default_text;
 
-	new_page_link = `https://docs.google.com/forms/d/e/1FAIpQLSdlKV-BTigb_wjBhug6vJKieOikpwyijXOf6YS2CmHdvRtz7g/viewform?usp=pp_url&entry.691033026=${text}`
-	window.location.replace(new_page_link);
+    new_page_link = `https://docs.google.com/forms/d/e/1FAIpQLSdlKV-BTigb_wjBhug6vJKieOikpwyijXOf6YS2CmHdvRtz7g/viewform?usp=pp_url&entry.691033026=${text}`
+    window.location.replace(new_page_link);
 }
 
-var pointer = document.querySelector("#preview"),
-	pointerBox = pointer.getBoundingClientRect(),
-	centerPoint = window.getComputedStyle(pointer).transformOrigin,
-	centers = centerPoint.split(" ");
 
-function rotatePointer(e) {
-	var pointerEvent = e;
+// -- ROTATING ELEMENT STUFF --
+const rotatingElement = document.getElementById("preview");
+var rotatingElementMouseDown = false;
+rotatingElement.dataset.prevPercentage = 0;
 
-	if (e.targetTouches && e.targetTouches[0]) {
-		e.preventDefault();
-		pointerEvent = e.targetTouches[0];
-		mouseX = pointerEvent.pageX;
-		mouseY = pointerEvent.pageY;
-	} else {
-		mouseX = e.clientX,
-			mouseY = e.clientY;
-	}
-
-	if (
-		mouseX >= pointerBox.left &&
-		mouseX <= pointerBox.right &&
-		mouseY >= pointerBox.top &&
-		mouseY <= pointerBox.bottom
-	) {
-		var centerY = pointerBox.top + parseInt(centers[1]) - window.pageYOffset,
-			centerX = pointerBox.left + parseInt(centers[0]) - window.pageXOffset,
-			radians = Math.atan2(mouseX - centerX, mouseY - centerY),
-			degrees = (radians * (180 / Math.PI) * -1) + 180;
-		pointer.style.transform = 'rotate(' + degrees + 'deg)';
-	}
+rotatingElement.onmousedown = e => {
+    // Set current mouse postion
+    rotatingElement.dataset.mouseDownAt = e.clientX;
+    rotatingElementMouseDown = true;
 }
 
-window.addEventListener('mousemove', rotatePointer);
-window.addEventListener('touchmove', rotatePointer);
-window.addEventListener('touchstart', rotatePointer);
+window.onmouseup = e => {
+    // Reset mouse position
+    // Set current percentage along scale
+    if (rotatingElementMouseDown) {
+        rotatingElement.dataset.mouseDownAt = 0;
+        rotatingElement.dataset.prevPercentage = rotatingElement.dataset.nextPercentage;
+        rotatingElementMouseDown = false;
+    }
+}
+
+window.onmousemove = e => {
+    if (rotatingElementMouseDown) animateRotatingElement(e, rotatingElement);
+}
+
+function animateRotatingElement(e, rotatingElement) {
+    // If no change, return
+    if (rotatingElement.dataset.mouseDownAt === 0) return;
+
+    // Set slider size
+    const maxDelta = window.innerWidth / 5;
+
+    // As mouse moves, get change in position relative to starting position
+    const mouseDelta = parseFloat(rotatingElement.dataset.mouseDownAt) - e.clientX;
+
+    // Calculate percentage over total
+    const percentage = mouseDelta * 100 / maxDelta;
+    const nextPercentage = parseFloat(rotatingElement.dataset.prevPercentage) + percentage;
+
+    // Save percentage for next mouse scroll after release
+    // to prevent reset of element on new mouse click
+    rotatingElement.dataset.nextPercentage = nextPercentage;
+
+    // Animate translation of element
+    rotatingElement.animate({
+        transform: `rotate(${nextPercentage}deg)`
+    }, {
+        duration: 500,
+        fill: "forwards"
+    })
+}
